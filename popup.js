9b4bae1cdf8306e304f3539a37feaac131126d52
Dhivya vijay <139950292+Dhivya-Vijay@users.noreply.github.com>
2023-08-13 20:19:57 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,14 @@
+var Name =prompt("Name")
+var str1="I am"
+
+var Age = Number (prompt("Age"))
+var str2=","
+var str3="year old"
+
+var Qualification = prompt("Qualification")
+var str4="My Qualification is"
+
+var Gender = prompt("Gender")
+var str5 = "and am"
+
+console.log(`${str1} ${Name} ${str2} ${Age} ${str3} ${str4} ${Qualification} ${str5} ${Gender}`)
